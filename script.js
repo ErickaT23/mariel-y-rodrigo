ce9ed054333cb6ad416954f45e7ddeab467d553e
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2024-08-08 16:34:46 -0600

    v1

--- script.js
+++ script.js
@@ -78,7 +78,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-
 // Función para mostrar la información del invitado
 function displayGuestInfo(index) {
     const guest = guests[index];
@@ -88,33 +87,6 @@ function displayGuestInfo(index) {
     document.getElementById('children').textContent = guest.children;
 }
 
-// Funciones del carrusel
-function showSlide(index, carouselId) {
-    const slides = document.querySelectorAll(`#${carouselId}-carousel .carousel-item`);
-    if (index >= slides.length) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = index;
-    }
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(${(i - currentSlide) * 100}%)`;
-    });
-}
-
-function nextSlide(carouselId) {
-    showSlide(currentSlide + 1, carouselId);
-}
-
-function prevSlide(carouselId) {
-    showSlide(currentSlide - 1, carouselId);
-}
-
-// Función para dar la vuelta a las tarjetas
-function flipCard(card) {
-    card.classList.toggle('flipped');
-}
 
 // Funciones para los buenos deseos
 function toggleWishes() {
